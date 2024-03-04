987e4d01899c9e8c9e256473991a2c244a79f38e
ButruilleG <gabriel.butruille@gmail.com>
2024-03-04 13:50:21 -0600

    working on encapsulation

--- run_jinja.py
+++ run_jinja.py
@@ -1,7 +1,9 @@
 from jinja2 import Environment, FileSystemLoader
 template_dir = "jinja_output"
 cql_to_jinja_source = "DemoMeasure_1.0"
-env = Environment(loader=FileSystemLoader(template_dir))
+env = Environment(loader = FileSystemLoader(template_dir))
+env.trim_blocks = True
+env.lstrip_blocks = True
 template = env.get_template(cql_to_jinja_source)
 print(template.render())
 # ".a()" should be replaced with whatever module we actually want to call
