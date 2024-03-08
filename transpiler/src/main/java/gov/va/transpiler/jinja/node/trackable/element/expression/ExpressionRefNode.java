c72386483bb063e49970ddf3317561a07b1eea1d
ButruilleG <gabriel.butruille@gmail.com>
2024-03-08 14:43:58 -0600

    reference support

--- transpiler/src/main/java/gov/va/transpiler/jinja/node/trackable/element/expression/ExpressionRefNode.java
+++ transpiler/src/main/java/gov/va/transpiler/jinja/node/trackable/element/expression/ExpressionRefNode.java
@@ -1,12 +1,13 @@
 package gov.va.transpiler.jinja.node.trackable.element.expression;
 
+import java.util.Map;
+
 import org.hl7.elm.r1.ExpressionRef;
 
 import gov.va.transpiler.jinja.state.State;
 import gov.va.transpiler.jinja.node.TranspilerNode;
 import gov.va.transpiler.jinja.node.trackable.element.expressiondef.ExpressionDefNode;
 import gov.va.transpiler.jinja.node.utilityinterfaces.ReferenceNode;
-import gov.va.transpiler.jinja.printing.Segment;
 
 public class ExpressionRefNode extends ExpressionNode<ExpressionRef> implements ReferenceNode {
 
@@ -38,7 +39,9 @@ public class ExpressionRefNode extends ExpressionNode<ExpressionRef> implements 
     }
 
     @Override
-    public Segment toSegment() {
-        return new Segment((prefix == null ? "" : prefix + ".") + referenceName() + "(state)");
+    protected Map<String, String> getSimpleArgumentMap() {
+        var map = super.getSimpleArgumentMap();
+        map.put("'reference'", (prefix == null ? "" : prefix + ".") + referenceName());
+        return map;
     }
 }
