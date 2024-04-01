926b5ae35c0491ec1019a92c7517c22f0ff97052
ButruilleG <gabriel.butruille@gmail.com>
2024-04-01 17:54:41 -0500

    alias and interval node

---
+++ transpiler/src/main/java/gov/va/transpiler/jinja/node/trackable/element/expression/IntervalNode.java
@@ -0,0 +1,73 @@
+package gov.va.transpiler.jinja.node.trackable.element.expression;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.hl7.elm.r1.Interval;
+
+import gov.va.transpiler.jinja.node.CQLEquivalent;
+import gov.va.transpiler.jinja.node.TranspilerNode;
+import gov.va.transpiler.jinja.state.State;
+
+public class IntervalNode extends ExpressionNode<Interval> {
+
+    TranspilerNode highNode;
+    TranspilerNode highClosedExpressionNode;
+    TranspilerNode lowNode;
+    TranspilerNode lowClosedExpressionNode;
+
+    public IntervalNode(State state, Interval cqlEquivalent) {
+        super(state, cqlEquivalent);
+    }
+    
+    public void addChild(TranspilerNode child) {
+        if (child instanceof CQLEquivalent) {
+            var cqlEquivalentChild = (CQLEquivalent<?>) child;
+            if (cqlEquivalentChild.getCqlEquivalent() == getCqlEquivalent().getHigh()) {
+                if (highNode == null) {
+                    highNode = child;
+                } else {
+                    super.addChild(child);
+                }
+            } else if (cqlEquivalentChild.getCqlEquivalent() == getCqlEquivalent().getHighClosedExpression()) {
+                if (highClosedExpressionNode == null) {
+                    highClosedExpressionNode = child;
+                } else {
+                    super.addChild(child);
+                }
+            } else if (cqlEquivalentChild.getCqlEquivalent() == getCqlEquivalent().getLow()) {
+                if (lowNode == null) {
+                    lowNode = child;
+                } else {
+                    super.addChild(child);
+                }
+            } else if (cqlEquivalentChild.getCqlEquivalent() == getCqlEquivalent().getHighClosedExpression()) {
+                if (lowClosedExpressionNode == null) {
+                    lowClosedExpressionNode = child;
+                } else {
+                    super.addChild(child);
+                }
+            } else {
+                super.addChild(child);
+            }
+        } else {
+            super.addChild(child);
+        }
+    }
+
+    @Override
+    public int allowedNumberOfChildren() {
+        return 0;
+    }
+
+    @Override
+    protected Map<String, List<TranspilerNode>> getComplexArgumentMap() {
+        var map = super.getComplexArgumentMap();
+        map.put("'high'", Collections.singletonList(highNode));
+        map.put("'highClosedExpression'", Collections.singletonList(highClosedExpressionNode));
+        map.put("'low'", Collections.singletonList(lowNode));
+        map.put("'lowClosedExpression'", Collections.singletonList(lowClosedExpressionNode));
+        return map;
+    }
+}
---
+++ transpiler/src/main/java/gov/va/transpiler/jinja/node/trackable/element/expression/operatorexpression/AliasRefNode.java
@@ -0,0 +1,28 @@
+package gov.va.transpiler.jinja.node.trackable.element.expression.operatorexpression;
+
+import java.util.Map;
+
+import org.hl7.elm.r1.AliasRef;
+
+import gov.va.transpiler.jinja.node.trackable.element.expression.ExpressionNode;
+import gov.va.transpiler.jinja.state.State;
+
+public class AliasRefNode extends ExpressionNode<AliasRef> {
+    // TODO: include identifier source as nested operator tree inside output
+
+    public AliasRefNode(State state, AliasRef cqlEquivalent) {
+        super(state, cqlEquivalent);
+    }
+
+    @Override
+    protected Map<String, String> getSimpleArgumentMap() {
+        var map = super.getSimpleArgumentMap();
+        map.put("'name'", "'" + getCqlEquivalent().getName() + "'");
+        return map;
+    }
+
+    @Override
+    public int allowedNumberOfChildren() {
+        return 0;
+    }
+}
