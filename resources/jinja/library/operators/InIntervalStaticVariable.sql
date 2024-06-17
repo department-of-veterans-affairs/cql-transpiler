cce76e5e02a0ceccd6042a78b14c34b9dee63061
ButruilleG <gabriel.butruille@gmail.com>
2024-06-17 17:12:18 -0500

    dependency fixes

--- resources/jinja/library/operators/InIntervalStaticVariable.sql
+++ resources/jinja/library/operators/InIntervalStaticVariable.sql
@@ -7,6 +7,7 @@
 {% from "library/globals/OperatorHandlerStaticVariable.sql" import OperatorHandlerStaticVariableInit %}
 {% from "library/globals/OperatorClass.sql" import OperatorClassInit %}
 {% from "library/globals/DataTypeEnum.sql" import DataTypeEnumInit %}
+{% from "library/globals/IntervalStaticVariables.sql" import IntervalStaticVariablesInit %}
 
 {%- macro InIntervalPrint(environment, this, state, arguments) -%}
 {{ environment.OperatorHandler.print(environment, this, state, arguments['left'])}} BETWEEN SELECT {{ environment.intervalStart }} FROM {{ environment.OperatorHandler.print(environment, this, state, arguments['right']) }} AND SELECT {{ environment.intervalEnd }} FROM {{ environment.OperatorHandler.print(environment, this, state, arguments['right']) }}
@@ -14,9 +15,10 @@
 
 {% macro InIntervalStaticVariableInit(environment) %}
 {# initialize prerequisites #}
-{%-   do OperatorHandlerStaticVariableInit(environment) %}
-{%-   do OperatorClassInit(environment) %}
-{%-   do DataTypeEnumInit(environment) %}
+{%-     do OperatorHandlerStaticVariableInit(environment) %}
+{%-     do OperatorClassInit(environment) %}
+{%-     do DataTypeEnumInit(environment) %}
+{%-     do IntervalStaticVariablesInit(environment) %}
 {# initialize member variables #}
 {%-     set InInterval = namespace() %}
 {%-     set environment.InInterval = InInterval %}
--- resources/jinja/library/operators/IntervalStaticVariable.sql
+++ resources/jinja/library/operators/IntervalStaticVariable.sql
@@ -7,6 +7,7 @@
 {% from "library/globals/OperatorHandlerStaticVariable.sql" import OperatorHandlerStaticVariableInit %}
 {% from "library/globals/OperatorClass.sql" import OperatorClassInit %}
 {% from "library/globals/DataTypeEnum.sql" import DataTypeEnumInit %}
+{% from "library/globals/IntervalStaticVariables.sql" import IntervalStaticVariablesInit %}
 
 {%- macro IntervalPrint(environment, this, state, arguments) -%}
 SELECT struct({{ environment.OperatorHandler.print(environment, this, state, arguments['high'])}} as {{ environment.intervalEnd }}, {{ environment.OperatorHandler.print(environment, this, state, arguments['low']) }} as {{ environment.intervalStart }}) {{ printSingleValueColumnName() }}
@@ -14,9 +15,10 @@ SELECT struct({{ environment.OperatorHandler.print(environment, this, state, arg
 
 {% macro IntervalStaticVariableInit(environment) %}
 {# initialize prerequisites #}
-{%-   do OperatorHandlerStaticVariableInit(environment) %}
-{%-   do OperatorClassInit(environment) %}
-{%-   do DataTypeEnumInit(environment) %}
+{%-     do OperatorHandlerStaticVariableInit(environment) %}
+{%-     do OperatorClassInit(environment) %}
+{%-     do DataTypeEnumInit(environment) %}
+{%-     do IntervalStaticVariablesInit(environment) %}
 {# initialize member variables #}
 {%-     set Interval = namespace() %}
 {%-     set environment.Interval = Interval %}
