a125900ee2f2146eceaaaa583d0168b36e8d5ef5
ButruilleG <gabriel.butruille@gmail.com>
2024-06-21 16:39:04 -0500

    more whitespace fixes

--- resources/jinja/library/operators/PropertyStaticVariable.sql
+++ resources/jinja/library/operators/PropertyStaticVariable.sql
@@ -1,32 +1,32 @@
-{#
+{#-
     Environment prerequisites:
         * OperatorHandlerStaticVariable.sql
         * OperatorClass.sql
         * DataTypeEnum.sql
 #}
-{% from "library/globals/OperatorHandlerStaticVariable.sql" import OperatorHandlerStaticVariableInit %}
-{% from "library/globals/OperatorClass.sql" import OperatorClassInit %}
-{% from "library/globals/DataTypeEnum.sql" import DataTypeEnumInit %}
+{%- from "library/globals/OperatorHandlerStaticVariable.sql" import OperatorHandlerStaticVariableInit %}
+{%- from "library/globals/OperatorClass.sql" import OperatorClassInit %}
+{%- from "library/globals/DataTypeEnum.sql" import DataTypeEnumInit %}
 
 {%- macro PropertyPrint(environment, this, state, arguments) -%}
-{%-     if arguments['child'] != none %}
-{{ environment.OperatorHandler.print(environment, this, state, arguments['child']) }}.{{ arguments['path'] }}
-{%-     elif arguments['path'] != none %}
-{{ arguments['scope'] }}.{{ arguments['path'] }}
-{%-     else %}
-{{ arguments['scope'] }}
-{%-     endif %}
+    {%- if arguments['child'] != none -%}
+        {{ environment.OperatorHandler.print(environment, this, state, arguments['child']) }}.{{ arguments['path'] }}
+    {%- elif arguments['path'] != none -%}
+        {{ arguments['scope'] }}.{{ arguments['path'] }}
+    {%- else -%}
+        {{ arguments['scope'] }}
+    {%- endif %}
 {%- endmacro %}
 
-{% macro PropertyStaticVariableInit(environment) %}
-{# initialize prerequisites #}
-{%-   do OperatorHandlerStaticVariableInit(environment) %}
-{%-   do OperatorClassInit(environment) %}
-{%-   do DataTypeEnumInit(environment) %}
-{# initialize member variables #}
-{%-     set Property = namespace() %}
-{%-     set environment.Property = Property %}
-{%-     do environment.OperatorClass.construct(environment, none, environment.Property) %}
-{%-     set Property.defaultDataType = environment.DataTypeEnum.ENCAPSULATED %}
-{%-     set Property.print = PropertyPrint %}
+{%- macro PropertyStaticVariableInit(environment) %}
+    {#- initialize prerequisites #}
+    {%- do OperatorHandlerStaticVariableInit(environment) %}
+    {%- do OperatorClassInit(environment) %}
+    {%- do DataTypeEnumInit(environment) %}
+    {# initialize member variables #}
+    {%- set Property = namespace() %}
+    {%- set environment.Property = Property %}
+    {%- do environment.OperatorClass.construct(environment, none, environment.Property) %}
+    {%- set Property.defaultDataType = environment.DataTypeEnum.ENCAPSULATED %}
+    {%- set Property.print = PropertyPrint %}
 {%- endmacro %}
--- resources/jinja/library/operators/QueryStaticVariable.sql
+++ resources/jinja/library/operators/QueryStaticVariable.sql
@@ -1,46 +1,45 @@
-{#
+{#-
     Environment prerequisites:
         * OperatorHandlerStaticVariable.sql
         * OperatorClass.sql
         * DataTypeEnum.sql
 #}
-{% from "library/globals/OperatorHandlerStaticVariable.sql" import OperatorHandlerStaticVariableInit %}
-{% from "library/globals/OperatorClass.sql" import OperatorClassInit %}
-{% from "library/globals/DataTypeEnum.sql" import DataTypeEnumInit %}
+{%- from "library/globals/OperatorHandlerStaticVariable.sql" import OperatorHandlerStaticVariableInit %}
+{%- from "library/globals/OperatorClass.sql" import OperatorClassInit %}
+{%- from "library/globals/DataTypeEnum.sql" import DataTypeEnumInit %}
 
 {%- macro QueryPrint(environment, this, state, arguments) -%}
-{%      set state.insideQuery = true %}
-{%-     set previousCoercionInstructions = state.coercionInstructions %}
-{%-     set state.coercionInstructions = {} %}
-{%-     if arguments['letClauseList']|length > 0 %}
-LET {{ environment.printOperatorsFromList(environment, state, arguments['letClauseList'], ", ") }} 
-{%-     endif %}
-SELECT
-{%-     if arguments['returnClause'] == none %}
- *
-{%-     else %}
- {{ environment.OperatorHandler.print(environment, this, state, arguments['returnClause']) }} 
-{%-     endif %}
- FROM ({{ environment.printOperatorsFromList(environment, state, arguments['children'], ", ") }})
-{%-     if arguments['where'] != none %}
- WHERE {{ environment.OperatorHandler.print(environment, this, state, arguments['where']) }}
-{%-     endif %}
-{%-     if arguments['sortClause'] != none %}
- {{ environment.OperatorHandler.print(environment, this, state, arguments['sortClause']) }}
-{%-     endif %}
-{%-     set state.coercionInstructions = previousCoercionInstructions %}
-{%-     set state.insideQuery = false %}
+    {% set state.insideQuery = true %}
+    {% set previousCoercionInstructions = state.coercionInstructions %}
+    {% set state.coercionInstructions = {} %}
+    {% if arguments['letClauseList']|length > 0 -%}
+        LET {{ environment.printOperatorsFromList(environment, state, arguments['letClauseList'], ", ") }} 
+    {%- endif -%}
+    SELECT
+    {%- if arguments['returnClause'] == none -%}
+        *
+    {%- else -%}
+        {{ environment.OperatorHandler.print(environment, this, state, arguments['returnClause']) }} 
+    {%- endif -%}
+    FROM ({{ environment.printOperatorsFromList(environment, state, arguments['children'], ", ") }})
+    {%- if arguments['where'] != none -%}
+        WHERE {{ environment.OperatorHandler.print(environment, this, state, arguments['where']) }}
+    {%- endif %}
+    {%- if arguments['sortClause'] != none %} {{ environment.OperatorHandler.print(environment, this, state, arguments['sortClause']) }}
+    {%- endif %}
+    {%- set state.coercionInstructions = previousCoercionInstructions %}
+    {%- set state.insideQuery = false %}
 {%- endmacro %}
 
-{% macro QueryStaticVariableInit(environment) %}
-{# initialize prerequisites #}
-{%-   do OperatorHandlerStaticVariableInit(environment) %}
-{%-   do OperatorClassInit(environment) %}
-{%-   do DataTypeEnumInit(environment) %}
-{# initialize member variables #}
-{%-     set Query = namespace() %}
-{%-     set environment.Query = Query %}
-{%-     do environment.OperatorClass.construct(environment, none, environment.Query) %}
-{%-     set Query.defaultDataType = environment.DataTypeEnum.TABLE %}
-{%-     set Query.print = QueryPrint %}
+{%- macro QueryStaticVariableInit(environment) %}
+    {#- initialize prerequisites #}
+    {%- do OperatorHandlerStaticVariableInit(environment) %}
+    {%- do OperatorClassInit(environment) %}
+    {%- do DataTypeEnumInit(environment) %}
+    {#- initialize member variables #}
+    {%- set Query = namespace() %}
+    {%- set environment.Query = Query %}
+    {%- do environment.OperatorClass.construct(environment, none, environment.Query) %}
+    {%- set Query.defaultDataType = environment.DataTypeEnum.TABLE %}
+    {%- set Query.print = QueryPrint %}
 {%- endmacro %}
