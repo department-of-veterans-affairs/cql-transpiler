6c1437628555a9f241d84744cbb323f6217af081
ButruilleG <gabriel.butruille@gmail.com>
2024-06-27 16:00:42 -0500

    coercion fix

--- resources/jinja/library/operators/InIntervalStaticVariable.sql
+++ resources/jinja/library/operators/InIntervalStaticVariable.sql
@@ -10,7 +10,11 @@
 {%- from "library/globals/IntervalStaticVariables.sql" import IntervalStaticVariablesInit %}
 
 {%- macro InIntervalPrint(environment, this, state, arguments) -%}
-    {{ environment.OperatorHandler.print(environment, this, state, arguments['left'])}} BETWEEN SELECT {{ environment.intervalStart }} FROM {{ environment.OperatorHandler.print(environment, this, state, arguments['right']) }} AND SELECT {{ environment.intervalEnd }} FROM {{ environment.OperatorHandler.print(environment, this, state, arguments['right']) }}
+{# TODO: before selecting from right set coercion to encapsulated #}
+    {{ environment.OperatorHandler.print(environment, this, state, arguments['left'])}} BETWEEN
+    {%- set previousCoercionInstructions = state.coercionInstructions %}
+    {%- set state.coercionInstructions = { environment.DataTypeEnum.TABLE: environment.DataTypeEnum.ENCAPSULATED, environment.DataTypeEnum.SIMPLE: environment.DataTypeEnum.ENCAPSULATED } %} SELECT {{ environment.intervalStart }} FROM {{ environment.OperatorHandler.print(environment, this, state, arguments['right']) }} AND SELECT {{ environment.intervalEnd }} FROM {{ environment.OperatorHandler.print(environment, this, state, arguments['right']) }}
+    {%- set state.coercionInstructions = previousCoercionInstructions %}
 {%- endmacro %}
 
 {%- macro InIntervalStaticVariableInit(environment) %}
