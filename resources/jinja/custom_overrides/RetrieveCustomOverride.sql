9f52b2e443239aedb9c0a1a952149e4e53cba933
ButruilleG <gabriel.butruille@gmail.com>
2024-07-08 20:32:07 -0500

    fix for bad opening jinja bracket

--- resources/jinja/custom_overrides/RetrieveCustomOverride.sql
+++ resources/jinja/custom_overrides/RetrieveCustomOverride.sql
@@ -6,7 +6,7 @@
 {%- from "jinja_transpilation_libraries/sparksql/default/globals/OperatorHandlerStaticVariable.sql" import OperatorHandlerStaticVariableInit %}
 {%- from "jinja_transpilation_libraries/sparksql/default/RetrieveStaticVariable.sql" import RetrieveStaticVariableInit %}
 
-{%0 macro systemValueset() -%}
+{%- macro systemValueset() -%}
   WITH cy2024 AS (SELECT _c1 `oid`, _c0 `name`, _c2 `version`, _c8 `code`, _c9 `display`, _c10 `codeSystem`, _c11 `codeSystemOid`FROM CSV.`/mnt/zones/subenv/{{ env_var('WORKGROUP_SUBENV')|lower }}/specification/valueset/vsac/ec_eh_oqr_unique_vs_20230504.csv.gz`), all AS (SELECT t.oid, t.name, t.version, STRUCT(t.code, t.display, cs.url system) code FROM cy2024 t LEFT JOIN {{ source("fhirlake", "terminology__codesystem") }} cs ON CONCAT('urn:oid:', t.codeSystemOid) = cs.identifier[0].value) SELECT oid, FIRST(name) name, version, COLLECT_LIST(code) codes FROM all GROUP BY oid, version
 {%- endmacro %}
 
