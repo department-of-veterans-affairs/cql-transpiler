fab208e912e05424485cfdb3df0788dd21e83a54
ButruilleG <gabriel.butruille@gmail.com>
2024-08-20 16:40:06 -0500

    fix for valuesetref change

--- resources/jinja/custom_overrides/ValueSetRefCustomOverride.sql
+++ resources/jinja/custom_overrides/ValueSetRefCustomOverride.sql
@@ -7,7 +7,7 @@
 {%- from "library/operators/ValueSetRefStaticVariable.sql" import ValueSetRefStaticVariableInit %}
 
 {%- macro ValueSetRefPrintCustomOverride(environment, this, state, arguments) -%}
-    {{ arguments['referenceTo'][value].split(':')[2] }}
+    {{ arguments['referenceTo']['value'].split(':')[2] }}
 {%- endmacro %}
 
 {%- macro ValueSetRefCustomOverrideInit(environment) %}
