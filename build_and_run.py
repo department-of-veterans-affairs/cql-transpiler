6073b6ebf41023d85caa091d5d15185e53dd9e39
ButruilleG <gabriel.butruille@gmail.com>
2025-03-11 20:20:15 -0500

    changes hardcoded variables to arguments

--- build_and_run.py
+++ build_and_run.py
@@ -1,6 +1,6 @@
 import subprocess
-import sys
 import os
+from render_models import render_models
 
 def run_command(command):
     process = subprocess.Popen(command, shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -28,20 +28,35 @@ def main():
 
         # Run the Transpiler class with the specified arguments
         print("Running the Transpiler...")
+        cql_libraries_to_transpile = "CMS104-v12-0-000-QDM-5-6.cql,CMS506v7/CMS506-v7-0-000-QDM-5-6.cql"
         java_command = (
             f"java -cp {jar_path} gov.va.transpiler.jinja.Transpiler "
-            "librarySource=./resources/cql/ "
-            "jinjaTarget=./resources/jinja/ "
-            "targetLanguage=sparksql "
-            "printFunctions=false"
+            f"librarySource=./resources/cql/ "
+            f"jinjaTarget=./resources/jinja/ "
+            f"targetLanguage=sparksql "
+            f"printFunctions=false "
+            f"cqlLibrariesToTranspile={cql_libraries_to_transpile}"
         )
         if not run_command(java_command):
             raise Exception("Transpiler execution failed")
 
-        # Invoke the render_models.py script
-        print("Invoking render_models.py...")
-        if not run_command("python render_models.py"):
-            raise Exception("render_models.py execution failed")
+        # Set variables for render_models
+        resources_folder = "resources/"
+        jinja_subfolder = 'jinja/'
+        intermediate_ast_folders = [
+            'generated_models/MATGlobalCommonFunctions_7__0__000/', 
+            'generated_models/TJCOverall_7__1__000/', 
+            'generated_models/DischargedonAntithromboticTherapy_12__0__000/', 
+            'generated_models/MATGlobalCommonFunctionsQDM_8__0__000/', 
+            'generated_models/SafeUseofOpioidsConcurrentPrescribing_7__0__000/'
+        ]
+        target_subfolder = "transpiler_dbt_models/"
+        skip_failing_files = False
+        autoformat = True
+
+        # Invoke the render_models function
+        print("Invoking render_models function...")
+        render_models(resources_folder, jinja_subfolder, intermediate_ast_folders, target_subfolder, skip_failing_files, autoformat)
 
     except Exception as e:
         print(e)
